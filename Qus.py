f2d766b752f9e39b9c95992c5b9e48da7b0f5860
拚拚 <robin92062574@gmail.com>
2023-12-19 03:03:14 +0800

    almost

--- Qus.py
+++ Qus.py
@@ -10,18 +10,23 @@ from sklearn.tree import DecisionTreeClassifier #決策樹模型
 # from sklearn.model_selection import GridSearchCVf
 
 from sklearn.impute import SimpleImputer
+# 降維
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans,DBSCAN #分群
 from sklearn.preprocessing import StandardScaler, OneHotEncoder , RobustScaler , OrdinalEncoder,MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
+# 關聯
+
 
 # 模型評分
 from sklearn.metrics import accuracy_score, r2_score, mean_squared_error,silhouette_score
 # 畫畫
 import matplotlib
+# inner
 from data import Data
+
 class Qus(Data):
 
     def __init__(self, source="Customer Data/customer_data.csv") -> pd.DataFrame:
@@ -45,18 +50,22 @@ class Qus(Data):
         # Los Angeles  San Diego Sacramento
         top3_city = ['Los Angeles','San Diego','Sacramento']
 
-        df = df[['城市'] in top3_city]
-        # 網路、電話
-        df_net = df[['網路服務','網路連線類型','平均下載量( GB)','線上安全服務','線上備份服務','設備保護計劃','技術支援計劃','電視節目','電影節目','音樂節目','無限資料下載','合約類型','支付帳單方式','每月費用','總費用','總退款','額外數據費用', '額外長途費用']]
-
+        df = df[df['城市'].isin(top3_city)]
+        # 數值特徵資料
+        df_num = df[['城市','每月費用','總費用','總退款','總收入']]
+        df_num = df_num.groupby('城市').sum()
+        # 類別特徵資料
+        df_cat = df[['城市','網路服務','網路連線類型','線上安全服務','線上備份服務','設備保護計劃','技術支援計劃','電視節目','電影節目','音樂節目','無限資料下載','合約類型','支付帳單方式',]]
+        df_cat = df_cat.groupby('城市').value_counts()
         """畫圖"""
         df_total.value_counts()
         df_total = df_total.reset_index()
-        bar_pic = df.plot.bar()
+        df_num.plot.bar()
         plt.title('總收入前三名城市其數據')
         plt.ylabel('金額');plt.xlabel('城市')
         plt.grid(axis='y')
         plt.show()
+        df_cat.plot.bar()
         """
         過程：
         先將清洗完的資料以‘城市’為組別進行加總並透過排序取出總收入前三名的城市
@@ -117,7 +126,124 @@ class Qus(Data):
         accuracy = accuracy_score(y_test, y_pred)
 
         print(f'模型準確度：{accuracy}')
+    def Q4(self):
+        import pandas as pd
+        import matplotlib.pyplot as plt
+        from sklearn.cluster import KMeans
+        from mlxtend.frequent_patterns import apriori
+        from mlxtend.frequent_patterns import association_rules
+        from matplotlib.font_manager import FontProperties
+        # 讀取資料
+        df = pd.read_csv('./change_Final.csv', encoding='utf-8')
 
+        feature = df[['經度','緯度 ']]
+
+        # 使用K-means演算法分成兩群
+        kmeans = KMeans(n_clusters=2, random_state=42)
+        df['Cluster'] = kmeans.fit_predict(feature)
+
+        # 顯示分群結果
+        print(df[['客戶編號', '緯度 ', '經度', 'Cluster']])
+        df[['客戶編號', '緯度 ', '經度', 'Cluster']].to_csv("5.csv")
+
+        # 繪製分群結果的散點圖
+        plt.scatter(df['緯度 '], df['經度'], c=df['Cluster'], cmap='viridis')
+        plt.title('geographical grouping')
+        plt.xlabel('latitude')
+        plt.ylabel('longitude ')
+        plt.show()
+        plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+        plt.rcParams['axes.unicode_minus'] = False
+
+        # 透過群組分類取得兩個群的資料
+        group_0_data = df[df['Cluster'] == 0]
+        group_1_data = df[df['Cluster'] == 1]
+
+        # 比較年齡的差異
+        age_comparison = pd.DataFrame({
+            'Group 0': [group_0_data['年齡'].mean()],
+            'Group 1': [group_1_data['年齡'].mean()]
+        }, index=['平均年齡'])
+
+        print("\n平均年齡比較:")
+        print(age_comparison)
+        plt.bar(age_comparison.columns, age_comparison.iloc[0], color=['lightcoral', 'lightskyblue'])
+        plt.xlabel('群組')
+        plt.ylabel('平均年齡')
+        plt.title('群組間年齡比較')
+        plt.show()
+        plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+        plt.rcParams['axes.unicode_minus'] = False
+
+        # 比較婚姻狀況的差異
+        marital_status_comparison = pd.crosstab(df['婚姻'], df['Cluster'])
+        print("\n婚姻狀況比較:")
+        print(marital_status_comparison)
+        marital_status_comparison.plot(kind='bar', stacked=True, color=['lightcoral', 'lightskyblue'])
+        plt.xlabel('群組')
+        plt.ylabel('人數')
+        plt.title('群組間婚姻狀況比較')
+        plt.show()
+        plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+        plt.rcParams['axes.unicode_minus'] = False
+
+        # 比較扶養人數的差異
+        dependents_comparison = pd.DataFrame({
+            'Group 0': [group_0_data['扶養人數'].mean()],
+            'Group 1': [group_1_data['扶養人數'].mean()]
+        }, index=['平均扶養人數'])
+
+        print("\n平均扶養人數比較:")
+        print(dependents_comparison)
+        # 绘制圆饼图
+        plt.bar(dependents_comparison.columns, dependents_comparison.iloc[0], color=['lightcoral', 'lightskyblue'])
+        plt.xlabel('群組')
+        plt.ylabel('平均扶養人數')
+        plt.title('群組間平均扶養人數比較')
+        plt.show()
+        plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+        plt.rcParams['axes.unicode_minus'] = False
+
+        # 比較電話服務的差異
+        promotion_comparison = pd.crosstab(df['電話服務 '], df['Cluster'])
+        print("\n電話服務比較:")
+        print(promotion_comparison)
+        promotion_comparison.plot(kind='bar', stacked=True, color=['lightcoral', 'lightskyblue'])
+        plt.xlabel('群組')
+        plt.ylabel('人數')
+        plt.title('群組間電話服務比較')
+        plt.show()
+        plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+        plt.rcParams['axes.unicode_minus'] = False
+
+        # 選擇 Group 0 的數據
+        group_0_data = df[df['Cluster'] == 0]
+
+        # 選擇相關特徵
+        selected_features = ['線上安全服務', '線上備份服務', '設備保護計劃', '技術支援計劃']
+        selected_features1 = ['電視節目', '電影節目', '音樂節目', '無限資料下載']
+
+        # 將特徵轉換成布林值類型
+        group_0_data[selected_features] = group_0_data[selected_features].applymap(lambda x: True if x == 'Yes' else False)
+        group_0_data[selected_features1] = group_0_data[selected_features1].applymap(lambda x: True if x == 'Yes' else False)
+
+        # 使用 Apriori 算法找出頻繁項集
+        frequent_itemsets = apriori(group_0_data[selected_features], min_support=0.2, use_colnames=True)
+        frequent_itemsets1 = apriori(group_0_data[selected_features1], min_support=0.3 , use_colnames=True)
+
+        # 根據頻繁項集生成關聯規則
+        rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.3)
+        rules1 = association_rules(frequent_itemsets1, metric="confidence", min_threshold=0.3)
+
+        # 顯示結果
+        print("頻繁項集:")
+        print(frequent_itemsets)
+        print("\n關聯規則:")
+        print(rules)
+        print("頻繁項集:")
+        print(frequent_itemsets1)
+        print("\n關聯規則:")
+        print(rules1)
     def Q5(self,way = 1):
         """
         \n題目:
@@ -165,7 +291,93 @@ class Qus(Data):
             self.__Q5_DBSCAN(data,transformed_data)
         else:
             raise KeyError(f"輸入參數{way}未知，請依照{way_dict}進行輸入")
+    def Q6(self):
+        """根據顧客年齡差異（分成老中青)，比較其使用公司服務的關聯規則的異同"""
+        df = self.read()
+        df = df[['年齡', '電話服務 ', '多線路服務', '網路服務', '線上安全服務',
+                '線上備份服務', '設備保護計劃', '技術支援計劃', '電視節目', '電影節目',
+                '音樂節目', '無限資料下載', '無紙化計費']]
 
+        def age_class(age):
+            if age <= 35:
+                return '青年'
+            elif age <= 55:
+                return '中年'
+            else:
+                return '老年'
+
+        # 創建col放年齡分類後的結果，刪除年齡欄
+        df['年齡層分類'] = df['年齡'].apply(age_class)
+        df = df.drop('年齡', axis=1)
+
+        # 建立不同年齡層的資料框架
+        df_young = df[df['年齡層分類'] == '青年'].copy()
+        df_middle = df[df['年齡層分類'] == '中年'].copy()
+        df_old = df[df['年齡層分類'] == '老年'].copy()
+
+        # 創建 LabelEncoder 實例
+        label_encoder = LabelEncoder()
+        # 將 '年齡層分類' 欄位轉換為數值型別
+        df_young['年齡層分類'] = label_encoder.fit_transform(df_young['年齡層分類'])
+        df_middle['年齡層分類'] = label_encoder.fit_transform(df_middle['年齡層分類'])
+        df_old['年齡層分類'] = label_encoder.fit_transform(df_old['年齡層分類'])
+
+        # 進行獨熱編碼
+        servers = ['電話服務 ', '多線路服務', '網路服務', '線上安全服務',
+                '線上備份服務', '設備保護計劃', '技術支援計劃', '電視節目',
+                '電影節目', '音樂節目', '無限資料下載', '無紙化計費']
+
+        df_young_encoded = pd.get_dummies(df_young, columns=servers)
+        df_middle_encoded = pd.get_dummies(df_middle, columns=servers)
+        df_old_encoded = pd.get_dummies(df_old, columns=servers)
+
+        # 進行關聯規則分析
+        frequent_itemsets_young = fpgrowth(df_young_encoded, min_support=0.5, use_colnames=True)
+        rules_young = association_rules(frequent_itemsets_young, metric="lift", min_threshold=1.1)
+        print("青年關聯規則：")
+        print(rules_young)
+        # make it as a data frame
+        df_rules = pd.DataFrame(rules_young)
+        df_rules.to_excel('./association_rules/rules_young_retail.xlsx')
+        print('-'*50)
+
+        frequent_itemsets_middle = fpgrowth(df_middle_encoded, min_support=0.5, use_colnames=True)
+        rules_middle = association_rules(frequent_itemsets_middle, metric="lift", min_threshold=1.1)
+        print("中年關聯規則：")
+        print(rules_middle)
+        # make it as a data frame
+        df_rules = pd.DataFrame(rules_middle)
+        df_rules.to_excel('./association_rules/rules_middle_retail.xlsx')
+        print('-'*50)
+
+        frequent_itemsets_old = fpgrowth(df_old_encoded, min_support=0.6, use_colnames=True)
+        rules_old = association_rules(frequent_itemsets_old, metric="lift", min_threshold=1.1)
+        print("老年關聯規則：")
+        print(rules_old)
+        # make it as a data frame
+        df_rules = pd.DataFrame(rules_old)
+        df_rules.to_excel('./association_rules/rules_old_retail.xlsx')
+        print('-'*50)
+        """
+        support用於衡量資料中包含特定項目集的頻率或出現次數。
+            support很高 -> 表示該項目集是一個常見的模式或關聯規則。
+
+        confidence用於評估一個規則的可靠程度或準確性。用來衡量「如果出現 A，則出現 B 的條件概率」。
+            =1 -> 當 A 出現時，B 一定會出現。
+            <1 -> 當 A 出現時，B 出現的機率較低。
+        
+        lift表示兩個事件（項目集）之間的相關性程度，特別是在資料挖掘和關聯規則分析中。
+            >1 -> A 和 B 之間存在正相關性，它們的出現是非獨立的。
+            =1 -> A 和 B 之間沒有相互關係，它們的出現是獨立的。
+            <1 -> A 和 B 之間存在負相關性，它們的出現是相斥的。
+        """
+
+
+        """
+        分析結果：
+        1. 顧客有使用'無限資料下載服務'時，也一定會使用'網路服務'。
+        2. 顧客同時使用'無限資料下載服務'與'電話服務'時，也一定會使用'網路服務'。
+        """
     def Q7(self):
         """第七題:以郵遞區號為主，顧客資料中使用電話服務的顧客，其特性可能為何"""
         data = self.read()
@@ -204,7 +416,7 @@ class Qus(Data):
         rf = RandomForestClassifier()
 
         # 使用網格搜索來尋找最佳參數組合
-        grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3)
+        grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, s=3)
         grid_search.fit(X, y)
 
         # 最佳參數
@@ -256,15 +468,15 @@ class Qus(Data):
         data_people = data[['Cluster', '年齡']].groupby('Cluster').count()
         print(data_age)
         print(data_people)
-        data_area = data[['Cluster', '總費用']].groupby("Cluster").sum()
+        data_area = data[['Cluster','年齡','郵遞區號', '總費用']].pivot_table(index="Cluster",columns="郵遞區號",values="總費用")
         print(data_area)
         data_service = data[['Cluster','郵遞區號','多線路服務']]
-        data_service = data_service.pivot_table(columns="Cluster",index="多線路服務",values="郵遞區號")
+        # data_service = data_service.pivot_table(columns="Cluster",index="郵遞區號",values="多線路服務")
         
         plt.figure(figsize=(20, 8))
 
-        # data_area.plot.bar()
-        data_service.plot.bar()
+        data_area.plot()
+        # data_service.plot.bar()
         print(data_service)
         plt.show()
         
@@ -345,48 +557,110 @@ class Qus(Data):
         plt.plot(range(2,group_num),res)
         plt.title('elbow');plt.xlabel('No. cluster')
         plt.show()
+    def Q8(self):
+        import pandas as pd
+        from sklearn.model_selection import train_test_split
+        from sklearn.tree import DecisionTreeClassifier
+        from sklearn.metrics import accuracy_score
+        from sklearn.preprocessing import MinMaxScaler
+        from sklearn.model_selection import GridSearchCV
+        from mlxtend.preprocessing import TransactionEncoder
+        from mlxtend.frequent_patterns import apriori
+        from mlxtend.frequent_patterns import association_rules
 
-    def test(self):
-        import matplotlib.pyplot as plt
-        import seaborn as sns
-        from sklearn.cluster import KMeans
-        from sklearn.preprocessing import StandardScaler
-        your_data = self.read()
-        # 選擇特徵
-        selected_features = ['平均下載量( GB)', '每月費用']
+        # 讀取CSV檔案
+        data = pd.read_csv('Customer Data/customer_data.csv', encoding='BIG5')
 
-        # 提取選擇的特徵
-        data_for_clustering = your_data[selected_features]
-        data_for_clustering = data_for_clustering.fillna(0)
-        # 特徵標準化
-        scaler = StandardScaler()
-        scaled_data = scaler.fit_transform(data_for_clustering)
+        # 選取所需的列進行分析
+        data = data[['年齡', '支付帳單方式', '郵遞區號']]
 
-        # 應用K-means，假設分3群
-        kmeans = KMeans(n_clusters=3, random_state=42)
-        kmeans.fit(scaled_data)
+        # 將年齡進行分層處理
+        bins = [0, 18, 30, 45, 60, float('inf')]
+        labels = ['少年', '青年', '中年', '壯年', '老年']
+        data['年齡層'] = pd.cut(data['年齡'], bins=bins, labels=labels, right=False)
 
-        # 新增分群結果到原始數據
-        your_data['Cluster'] = kmeans.labels_
+        # 對分類特徵進行獨熱編碼
+        data = pd.get_dummies(data, columns=['年齡層'])
 
-        # 繪製散點圖
-        plt.figure(figsize=(12, 8))
+        # 分割資料集為訓練集和測試集
+        X = data.drop(columns=['支付帳單方式'])  # 特徵
+        y = data['支付帳單方式']  # 目標變數
 
-        # 繪製散點圖
-        sns.scatterplot(x='平均下載量( GB)', y='每月費用', hue='Cluster', data=your_data, palette='viridis', s=100)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # 添加中心點
-        centers = scaler.inverse_transform(kmeans.cluster_centers_)  # 反標準化得到原始數據中心點
+        # 建立決策樹模型
+        clf = DecisionTreeClassifier(
+            random_state=42,
+            max_depth=3,
+            min_samples_split=2,
+            min_samples_leaf=5
+        )
 
-        # 添加中心點到散點圖
-        plt.scatter(centers[:, 0], centers[:, 1], marker='X', s=200, c='red', label='Cluster Centers')
+        # 訓練模型、預測和評估
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f'模型準確率：{accuracy}')
 
-        # 添加標籤和標題
-        plt.title('K-means 分群結果 with Cluster Centers')
-        plt.xlabel('平均下載量( GB)')
-        plt.ylabel('每月費用')
-        plt.legend()
-        plt.show()
+        print('*'*80)
 
-Qus().Q7()
+
+        # 正規化處理後的結果
+        scaler = MinMaxScaler()
+        X_train_normalized = scaler.fit_transform(X_train)
+        X_test_normalized = scaler.transform(X_test)
+
+        # 使用正規化後的數據構建決策樹模型
+        clf.fit(X_train_normalized, y_train)
+        y_pred_normalized = clf.predict(X_test_normalized)
+
+        accuracy_normalized = accuracy_score(y_test, y_pred_normalized)
+        print(f'模型準確率（正規化後）：{accuracy_normalized}')
+
+        print('*'*80)
+
+        # 使用 GridSearchCV 尋找最佳參數
+        param_grid = {
+            'max_depth': [3, 5, 7, 10, None],
+            'min_samples_split': [2, 5, 10, 15],
+            'min_samples_leaf': [1, 2, 3, 4, 5]
+        }
+
+        clf = DecisionTreeClassifier(random_state=42)
+        grid_search = GridSearchCV(clf, param_grid, cv=3, scoring='accuracy')
+        grid_search.fit(X_train, y_train)
+
+        best_params = grid_search.best_params_
+        print("最佳參數：", best_params)
+
+        best_clf = DecisionTreeClassifier(**best_params, random_state=38)
+        best_clf.fit(X_train, y_train)
+
+        best_predictions = best_clf.predict(X_test)
+        best_accuracy = accuracy_score(y_test, best_predictions)
+        print(f"最佳模型準確度：{best_accuracy}")
+
+        print('*'*80)
+
+        # 讀取資料並初始化空列表
+        records = []
+        for i in range(len(data)):
+            records.append([str(data.values[i, j]) for j in range(len(data.columns))])
+
+        # 將資料進行轉換和編碼
+        te = TransactionEncoder()
+        te_ary = te.fit(records).transform(records)
+        df = pd.DataFrame(te_ary, columns=te.columns_)
+
+        # 應用 apriori 挖掘頻繁項集
+        frequent_itemsets = apriori(df, min_support=0.5, use_colnames=True)
+
+        # 應用關聯規則分析並應用信心度過濾
+        association_rules(frequent_itemsets, metric="confidence", min_threshold=0.15)
+        rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+
+        # 將規則轉換為 DataFrame
+        df_rules = pd.DataFrame(rules)
+        print(df_rules)
+Qus().Q2()
 # Qus().test()
